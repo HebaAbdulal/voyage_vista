e51b0229488fac886563147f0e10263c034b60c5
Heba Abdulal <hebaabdulal24@gmail.com>
2024-05-31 12:22:00 +0000

    Install a cloudinary library

--- blog/settings.py
+++ blog/settings.py
@@ -17,9 +17,12 @@ import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
+
+development = os.environ.get('DEVELOPMENT', False)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -31,7 +34,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-hebaabdulal-voyagevista-dzvyodd221d.ws-eu114.gitpod.io']
-ALLOWED_HOSTS = ['ckz8780-django-voyagevista-app-19845c20e94f.herokuapp.com']
+ALLOWED_HOSTS = [
+    'ckz8780-django-voyagevista-app-19845c20e94f.herokuapp.com',
+    '8000-hebaabdulal-voyagevista-dzvyodd221d.ws-eu114.gitpod.io'
+]
 
 
 # Application definition
@@ -42,7 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'voyagevista',
 ]
 
@@ -61,7 +69,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,6 +136,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'cloudinary_storage.strorage.StaticHashedCloudinaryStorage'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+
+MEDIA_URL = '/media/'
+DEFAULT_FILE_STORAGE = 'cloudinary.storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
