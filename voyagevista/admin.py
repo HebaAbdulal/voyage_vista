2a28c093a1d6afd536938304fc1aea98e2f7f033
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-03 23:10:14 +0000

    Add approved field to Category model and configure admin interface

--- voyagevista/admin.py
+++ voyagevista/admin.py
@@ -1,13 +1,34 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    """
+    Admin configuration for the Post model.
+    """
+    prepopulated_fields = {'slug': ('title',)}
 
+    """
+Specifies the fields to be displayed in the admin interface, the fields used for search, 
+the field filters, the fields to be prepopulated, and the configuration for the rich-text editor.
+"""
+
+    list_filter = ('status', 'created_on', 'approved', 'author', 'updated_on', 'category')
     summernote_field = ('content')
+    list_display = ('status', 'created_on', 'approved', 'slug', 'title', 'author')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'created_on'
+    ordering = ('-created_on',)
 
 
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin configuration for the Category model.
+    """
+    prepopulated_fields = {'slug': ('name',)}
--- voyagevista/models.py
+++ voyagevista/models.py
@@ -5,7 +5,43 @@ from cloudinary.models import CloudinaryField
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
+class Category(models.Model):
+    """
+    Model representing a category for the blog posts.
+
+    Attributes:
+        name (CharField): The name of the category.
+        slug (SlugField): The slug for the category.
+    """
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
+    """
+    Model representing a blog post.
+
+    Attributes:
+        title (CharField): The title of the post.
+        slug (SlugField): The slug for the post.
+        author (ForeignKey): The author of the post, related to User model.
+        updated_on (DateTimeField): The date and time when the post was last updated.
+        content (TextField): The content of the post.
+        featured_image (CloudinaryField): The featured image of the post.
+        excerpt (TextField): A short excerpt from the post.
+        created_on (DateTimeField): The date and time when the post was created.
+        status (IntegerField): The status of the post, either Draft or Published.
+        likes (ManyToManyField): The users who liked the post.
+        shares (ManyToManyField): The users who shared the post.
+        saves (ManyToManyField): The users who saved the post.
+        approved (BooleanField): Whether the post is approved.
+        category (ForeignKey): The category of the post, related to Category model.
+    """
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
@@ -18,6 +54,8 @@ class Post(models.Model):
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
     shares = models.ManyToManyField(User, related_name='blog_shares', blank=True)
     saves = models.ManyToManyField(User, related_name='blog_saves', blank=True)
+    approved = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
 
     class Meta:
@@ -28,12 +66,21 @@ class Post(models.Model):
         return self.title
 
     def number_of_likes(self):
+        """
+        Returns the total number of likes for the post.
+        """
         return self.likes.count()
 
     def number_of_shares(self):
+        """
+        Returns the total number of shares for the post.
+        """
         return self.shares.count()
 
     def number_of_saves(self):
+        """
+        Returns the total number of saves for the post.
+        """
         return self.saves.count()
 
 class Comment(models.Model):
