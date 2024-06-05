c80bbc028864d2e154d58ebb3640df8121af25c6
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-05 13:09:14 +0000

    Add URL routing for voyagevista and home view

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernot/', include('django_summernote.urls')),
+    path('', include('voyagevista.urls'), name= 'voyagevista_urls'),
 ]
