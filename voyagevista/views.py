f12b0a4aff9aa4dcb4f59174de1c8ed06869fa08
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-05 23:21:24 +0000

    Add post_detail.html template with static tag

--- voyagevista/views.py
+++ voyagevista/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.views import generic
-from .models import Post, Category
+from .models import Post, Category, Comment
 from django.core.paginator import Paginator
 from .forms import CommentForm
 
