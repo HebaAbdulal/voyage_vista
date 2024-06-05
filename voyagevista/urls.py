94c8fb77f377ebbe8639e0d61aef6af16e6d9b7c
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-05 13:53:31 +0000

    Wire up URL pattern for category view in voyagevista app

--- voyagevista/urls.py
+++ voyagevista/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('category/<slug:category_slug>/', views.category_view, name='category'),
 ]
