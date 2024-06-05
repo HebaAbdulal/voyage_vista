309fc4d5bb88f1b6dcdc48ed82bcb95b3ba53a52
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-05 08:39:25 +0000

    Update index.html template to display categories and paginate posts

--- blog/urls.py
+++ blog/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('summernot/', include('django_summernote.urls'))
+    path('summernot/', include('django_summernote.urls')),
 ]
