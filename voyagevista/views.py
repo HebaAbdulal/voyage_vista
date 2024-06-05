bb8beed58d6738d67f2e96c0a721a962ab04bf21
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-05 21:55:56 +0000

    Retrieve active comments related to the post

--- voyagevista/views.py
+++ voyagevista/views.py
@@ -36,24 +36,3 @@ def post_detail(request, slug):
     """
     post = get_object_or_404(Post, slug=slug)
     comments = Comment.objects.filter(post=post, active=True)
-
-    # Increment view count
-    post.number_of_views += 1
-    post.save()
-
-    # Handle new comment submission
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-            new_comment = comment_form.save(commit=False)
-            new_comment.post = post
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
-
-    context = {
-        'post': post,
-        'comments': comments,
-        'comment_form': comment_form,
-    }
-    return render(request, 'post_detail.html', context)
