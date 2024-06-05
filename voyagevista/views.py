e8907c9ef8c6f5607eefaeec42479de7f673a775
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-05 13:56:30 +0000

    Implement category view in voyagevista app

--- voyagevista/views.py
+++ voyagevista/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import generic
-from .models import Post
+from .models import Post, Category
 
 
 class PostList(generic.ListView):
@@ -11,3 +11,12 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 4
+
+
+def category_view(request, category_slug):
+    """
+    View for displaying posts in a specific category.
+    """
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = Post.objects.filter(category=category, status=1).order_by('-created_on')
+    return render(request, 'category.html', {'category': category, 'posts': posts})
