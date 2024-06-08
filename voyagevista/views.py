78e226da0a0a8666243e31e550a116e59a85071c
Heba Abdulal <hebaabdulal24@gmail.com>
2024-06-08 12:46:26 +0000

    Add view count increment and comment handling with success message in post_detail view

--- voyagevista/views.py
+++ voyagevista/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404, redirect
 from django.views import generic
 from .models import Post, Category, Comment
 from django.core.paginator import Paginator
@@ -47,7 +47,16 @@ def post_detail(request, slug):
         if comment_form.is_valid():
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
-            new_comment.save()
+            if request.user.is_authenticated:
+                new_comment.name = request.user.username  # Set the comment name to the user's username
+                new_comment.email = request.user.email  # Set the comment email to the user's email
+                new_comment.active = False  # Set the comment to inactive (awaiting approval)
+                new_comment.save()
+                messages.add_message(request, messages.INFO, 'Your comment is awaiting approval.')
+            else:
+                new_comment.active = True  # Directly set to active if not logged in
+                new_comment.save()
+            return redirect('post_detail', slug=post.slug)
     else:
         comment_form = CommentForm()
 
