55199d5e4805c72f1331f60707fc69b4e820f4ca
Heba Abdulal <hebaabdulal24@gmail.com>
2024-07-05 09:49:15 +0000

    Configure VoyagevistaConfig with default_auto_field and signal import

--- voyagevista/apps.py
+++ voyagevista/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class VoyagevistaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'voyagevista'
+    def ready(self):
+        import voyagevista.signals
