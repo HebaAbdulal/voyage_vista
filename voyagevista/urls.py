b83d475889b237407d3d7c1e10e819ffa3136899
Heba Abdulal <hebaabdulal24@gmail.com>
2024-07-15 21:52:01 +0000

    Fix URL pattern for comment deletion

--- voyagevista/urls.py
+++ voyagevista/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import AddPostView, MyLikesView, MyCommentsView, MyBookmarksView, CommentEdit
+from .views import AddPostView, MyLikesView, MyCommentsView, MyBookmarksView, CommentEdit, CommentDeleteView
 
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path('add-post/', AddPostView.as_view(), name='add_post'),
     path('post/<slug:slug>/like/', views.PostLike.as_view(), name='post_like'),
     path('comment/<slug:slug>/<int:pk>/edit/', CommentEdit.as_view(), name='edit_comment'),
-    path('comment/<slug:slug>/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete_comment'),
+    path('comment/<slug:slug>/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
     path('post/<slug:slug>/delete/', views.DeletePostView.as_view(), name='delete_post'),
     path('my-likes/', MyLikesView.as_view(), name='my_likes'),
     path('my-comments/', MyCommentsView.as_view(), name='my_comments'),
