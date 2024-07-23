bd2c1393c67c1ce9e0fca9ff3f3d6e2598ea5138
Heba Abdulal <hebaabdulal24@gmail.com>
2024-07-23 14:43:30 +0000

    Update URL Patterns for Enhanced Post Management

--- voyagevista/urls.py
+++ voyagevista/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import AddPostView, MyLikesView, MyCommentsView, MyBookmarksView, CommentEdit, CommentDeleteView
+from .views import AddPostView, MyLikesView, MyCommentsView, MyBookmarksView, CommentEdit, CommentDeleteView, EditPostView, DeletePostView, MyPostsView
 
 
 urlpatterns = [
@@ -11,10 +11,12 @@ urlpatterns = [
     path('post/<slug:slug>/like/', views.PostLike.as_view(), name='post_like'),
     path('comment/<slug:slug>/<int:pk>/edit/', CommentEdit.as_view(), name='edit_comment'),
     path('comment/<slug:slug>/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete_comment'),
-    path('post/<slug:slug>/delete/', views.DeletePostView.as_view(), name='delete_post'),
+    path('post/<slug:slug>/delete/', DeletePostView.as_view(), name='delete_post'),
     path('my-likes/', MyLikesView.as_view(), name='my_likes'),
     path('my-comments/', MyCommentsView.as_view(), name='my_comments'),
     path('my-bookmarks/', MyBookmarksView.as_view(), name='my_bookmarks'),
     path('post/<slug:slug>/bookmark/', views.PostBookmark.as_view(), name='post_bookmark'),
     path('rate-post/<slug:post_slug>/', views.rate_post, name='rate_post'),
+    path('post/<slug:slug>/edit/', EditPostView.as_view(), name='edit_post'),
+    path('my-posts/', MyPostsView.as_view(), name='my_posts'),
 ]
