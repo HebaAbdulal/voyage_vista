b597d51e9a89107fb11946fb3d3c81bee91a5eb2
Heba Abdulal <hebaabdulal24@gmail.com>
2024-08-14 09:04:37 +0000

    Add ContactForm class with fields for name, email, and message

--- voyagevista/forms.py
+++ voyagevista/forms.py
@@ -29,3 +29,9 @@ class RatingForm(forms.ModelForm):
     class Meta:
         model = Rating
         fields = ['rating']
+
+
+class ContactForm(forms.Form):
+    name = forms.CharField(label='Name', max_length=100)
+    email = forms.EmailField(label='Email')
+    message = forms.CharField(label='Message', widget=forms.Textarea)
