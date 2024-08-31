95291baae60907027b60e83138ac7d67b3c8b7a3
Heba Abdulal <hebaabdulal24@gmail.com>
2024-08-31 15:25:31 +0000

    Add unit tests setup for views: create test user, category, and post

--- voyagevista/tests/test_views.py
+++ voyagevista/tests/test_views.py
@@ -2,3 +2,27 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from voyagevista.models import Post
 
+
+
+class TestViews(TestCase):
+    """
+    Unit tests for views in the VoyageVista app
+    """
+    def setUp(self):
+        """
+        Create test users and posts.
+        """
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        logged_in = self.client.login(username='testuser', password='testpass')
+        self.assertTrue(logged_in, "Login failed in setUp method")
+        
+        self.category = Category.objects.create(name='Test Category')
+        
+        self.post = Post.objects.create(
+            title="test title",
+            slug="test-title",
+            author=self.user,
+            content="Content of test post",
+            category=self.category,
+            status=1
+        )
